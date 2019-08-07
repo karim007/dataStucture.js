a2c36c29896d3a49517d6e6f78646bb58f2ee099
name <email>
2019-08-07 22:33:10 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let array= require("./src/dataStructures/array")
+let avl= require("./src/dataStructures/avl")
+let bts= require("./src/dataStructures/bts")
+let hashtable= require("./src/dataStructures/hashtable")
+let heap= require("./src/dataStructures/heap")
+let linkedList= require("./src/dataStructures/linkedList")
+
+
+export {array, avl, bts, hashtable,heap, linkedList}
+
--- src/dataStructures/dataStructures.js
+++
@@ -1,9 +0,0 @@
-let array= require("./array")
-let avl= require("./avl")
-let bts= require("./bts")
-let hashtable= require("./hashtable")
-let heap= require("./heap")
-let linkedList= require("./linkedList")
-
-
-export {array, avl, bts, hashtable,heap, linkedList}
